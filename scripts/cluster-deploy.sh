b4a7edd22ae0de6ec08f98d7bdd79165cc43ae02
root <root@graveyard.kubernetes.local>
2025-09-18 03:46:42 +0000

    yeah

--- scripts/cluster-deploy.sh
+++ scripts/cluster-deploy.sh
@@ -2,10 +2,4 @@
 sudo apt install -y wget curl vi openssl git ansible
 git clone [PUT REPO HERE]
 cd [REPO]
-ansible-playbook -i inventory.ini add-k8s-repo.yml
-ansible-playbook -i inventory.ini bootstrap-jumpbox.yml
-ansible-playbook -i inventory.ini bootstrap-control-planes.yml
-ansible-playbook -i inventory.ini bootstrap-workers.yml
-ansible-playbook -i inventory.ini
-ansible-playbook -i inventory.ini
-ansible-playbook -i inventory.ini
+
